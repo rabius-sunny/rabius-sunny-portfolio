b4e0b3efb2cd276f2251abb7cdf828d8df78924e
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-02-22 17:29:01 +0600

    add: tailwindcss custom color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,22 @@
 import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  theme: {},
+  theme: {
+    colors: {
+      primary: '#002a32',
+      main: '#0051a5',
+      text: '#a4d2d6',
+      heading: '#00d6d6',
+      white: colors.white,
+      sky: colors.sky,
+      pink: colors.pink
+    }
+  },
   plugins: []
 }
 export default config
