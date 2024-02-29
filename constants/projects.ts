62d6d652e47058dd0d80e2d7d836f6fb3983824e
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-01 04:05:14 +0600

    update: project

--- constants/projects.ts
+++ constants/projects.ts
@@ -10,12 +10,21 @@ const projects: TProject[] = [
   },
   {
     image: '/images/matrimony.png',
-    heading: 'jannatijuti.com',
+    heading: 'jannati juti',
     link: 'https://www.jannatijuti.com',
     subheading: 'Matrimonial website with Nextjs',
     description:
       'Find your perfect life partner whome you ever wanted leveraging efficient biodata search, hassle free information request and secure profile editing interface with super fast and edge technologies.',
     keywords: ['nextjs', 'redux', 'tailwindcss']
+  },
+  {
+    image: '/images/nextmart.png',
+    heading: 'next mart',
+    link: 'https://next-mart.vercel.app/shop',
+    subheading: 'E-commerce application with cart',
+    description:
+      'Dynamic e-commerce platform enabling users to discover, sort, and seamlessly add products to their cart for convenient shopping experiences.',
+    keywords: ['nextjs', 'redux', 'typescript']
   }
 ]
 export default projects
