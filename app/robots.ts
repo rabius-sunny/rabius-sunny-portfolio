578442a284f9b3b0002eac0ee8fd8f11938e059b
rabius-sunny <rabiussunnyc3x@gmail.com>
2026-05-06 00:24:58 +0600

    chore

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,6 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: 'https://rabiussunny.com/sitemap.xml',
-    host: 'https://rabiussunny.com'
+    sitemap: 'https://rabiussunny.com/sitemap.xml'
   }
 }
